f1ab14aa63c65b21eba7a5384a54fd7b38a5212c
Sagarnaga-Jesus <23308060610335@cetis61.edu.mx>
2026-05-18 08:49:56 -0500

    bd

--- src/database/finanzas.sql
+++ src/database/finanzas.sql
@@ -19,54 +19,24 @@
 CREATE DATABASE IF NOT EXISTS `ges_finanzas` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
 USE `ges_finanzas`;
 
--- Volcando estructura para tabla ges_finanzas.dinero
-CREATE TABLE IF NOT EXISTS `dinero` (
-    `id_dinero` int(11) NOT NULL AUTO_INCREMENT,
-    `presupuesto` float NOT NULL DEFAULT 0,
-    `id_usuario` int(11) NOT NULL,
-    PRIMARY KEY (`id_dinero`),
-    KEY `id_usuario` (`id_usuario`),
-    CONSTRAINT `dinero` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Volcando datos para la tabla ges_finanzas.dinero: ~0 rows (aproximadamente)
-DELETE FROM `dinero`;
-
--- Volcando estructura para tabla ges_finanzas.gastos
-CREATE TABLE IF NOT EXISTS `gastos` (
-    `id_gasto` int(11) NOT NULL AUTO_INCREMENT,
-    `titulo` varchar(50) DEFAULT NULL,
-    `descripcion` varchar(50) DEFAULT NULL,
-    `tipo_gasto` varchar(50) DEFAULT NULL,
-    `gasto_aprox` float DEFAULT NULL,
-    `dinero_usar` int(11) DEFAULT NULL,
-    `id_usuario` int(11) DEFAULT NULL,
-    PRIMARY KEY (`id_gasto`),
-    KEY `id_usuario` (`id_usuario`),
-    KEY `dinero` (`dinero_usar`) USING BTREE,
-    CONSTRAINT `dinero_gasto` FOREIGN KEY (`dinero_usar`) REFERENCES `dinero` (`id_dinero`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-    CONSTRAINT `usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Volcando datos para la tabla ges_finanzas.gastos: ~0 rows (aproximadamente)
-DELETE FROM `gastos`;
-
 -- Volcando estructura para tabla ges_finanzas.usuario
 CREATE TABLE IF NOT EXISTS `usuario` (
-    `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
-    `email` varchar(100) NOT NULL DEFAULT '',
-    `password` varchar(300) NOT NULL DEFAULT '',
-    `nombre` varchar(300) NOT NULL DEFAULT '',
-    `fecha_registro` datetime DEFAULT NULL,
-    `ultimo_registro` datetime DEFAULT NULL,
-    `foto` varchar(200) DEFAULT NULL,
-    PRIMARY KEY (`id_usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL DEFAULT '',
+  `password` varchar(300) NOT NULL DEFAULT '',
+  `nombre` varchar(300) NOT NULL DEFAULT '',
+  `fecha_registro` datetime DEFAULT NULL,
+  `ultimo_registro` datetime DEFAULT NULL,
+  `foto` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id_usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Volcando datos para la tabla ges_finanzas.usuario: ~0 rows (aproximadamente)
+-- Volcando datos para la tabla ges_finanzas.usuario: ~3 rows (aproximadamente)
 DELETE FROM `usuario`;
 INSERT INTO `usuario` (`id_usuario`, `email`, `password`, `nombre`, `fecha_registro`, `ultimo_registro`, `foto`) VALUES
-	(1, 'admin@gmail.com', '$2b$12$0kVL5yxk7kJ1M6QTLjxTleTJvDQm.YgxC6zTfpHXwTI9v5LyCWVbK', 'Admin', '2026-05-14 00:00:00', '2026-05-14 10:45:27', 'Default.webp');
+	(1, 'admin@gmail.com', '$2b$12$0kVL5yxk7kJ1M6QTLjxTleTJvDQm.YgxC6zTfpHXwTI9v5LyCWVbK', 'Admin', '2026-05-14 00:00:00', '2026-05-14 10:45:27', 'Default.webp'),
+	(2, '23308060610335@cetis61.edu.mx', '$2b$12$/BUePOsjWU6YJFzhnKNPeuojjwfTPJFihWqUgCr1WXurraGzU09vC', 'Antonio', '2026-05-18 00:00:00', '2026-05-18 08:42:57', 'Default.webp'),
+	(3, '23308060610320@cetis61.edu.mx', '$2b$12$2Cv1v6vx0Loo50hIi2aa0upN1X8OVwl3f0BOr/CUdwU8sYHNrMpIu', 'Luis Angel', '2026-05-18 00:00:00', NULL, 'Default.webp');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
