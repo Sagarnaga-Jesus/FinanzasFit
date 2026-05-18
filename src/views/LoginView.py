b62b9d2796c078a4330c970cdf2d9c30f7510828
Sagarnaga-Jesus <233308060610335@cetis61.edu.mx>
2026-05-17 20:46:29 -0600

    pendejadas

--- src/views/LoginView.py
+++ src/views/LoginView.py
@@ -2,7 +2,6 @@ import flet as ft
 from datetime import datetime
 import requests
 
-
 def LoginView(page: ft.Page, auth_controller):
     
     def ver_contra():
@@ -11,8 +10,6 @@ def LoginView(page: ft.Page, auth_controller):
         
     correo=(ft.TextField(label="Correo",autofocus=True, icon=ft.Icons.PERSON ))
     contra=(ft.TextField(label="Contraseña",suffix=ft.IconButton(icon=ft.Icons.VISIBILITY, on_click=ver_contra) ,password=True, autofocus=True, icon=ft.Icons.PASSWORD))
-    
-    
     
     def login_click(e):
         if not correo.value or not contra.value:
@@ -34,7 +31,8 @@ def LoginView(page: ft.Page, auth_controller):
     
     iniciar= ft.ElevatedButton("Iniciar sesión", on_click=login_click, width=350, bgcolor="cyan", color = "black", icon=(ft.Icon(ft.Icons.MAIL, color=ft.Colors.WHITE, size=25)))
     registrarse = ft.ElevatedButton("Crear una nueva cuenta", on_click=lambda _: page.go("/registro"), width=350, bgcolor="green", color = "black", icon=(ft.Icon(ft.Icons.ACCOUNT_BOX, color=ft.Colors.WHITE, size=25)))
-    olvidada =( ft.TextButton("¿Olvidaste la contraseña?", on_click=olvidado))
+    olvidada = ft.ElevatedButton("¿Olvidaste la contraseña?", on_click=lambda _: page.go("/olvidado"))
+    
     
     return ft.View(
         route="/",
