bdd862f923f7e72629cb4189517a8c3f4e70e037
RomoAlvaradoLuisAngel <23308060610320@cetis61.edu.mx>
2026-05-25 08:54:26 -0500

    tarjetita

--- src/views/GastosView.py
+++ src/views/GastosView.py
@@ -11,6 +11,21 @@ def GastosView(page: ft.Page):
     
     def regresar(e):
         page.go("/inicio")
+        
+    gasto = ft.Card(
+        content = [
+            ft.Text("Gastos", size=20, weight="bold"),
+            ft.Text("En esta seccion podras registra tus gastos realizados", size=14),
+            ft.Subtitle(content="Descripcion de gasto", size=20),
+            ft.Row([
+                ft.Column([
+                    ft.ElevatedButton(content="Añadir gasto", bgcolor=ft.Colors.GREEN_400, color=ft.Colors.WHITE),
+                    ft.ElevatedButton(content="Cambiar gasto", bgcolor=ft.Colors.BLUE_900, color=ft.Colors.WHITE),
+                    ft.ElevatedButton(content="Eliminar gasto", bgcolor=ft.Colors.RED_400, color=ft.Colors.WHITE),
+                ])
+            ])
+        ]
+    )
     
     
     return ft.View(
@@ -25,7 +40,8 @@ def GastosView(page: ft.Page):
         ),
         controls=[
             ft.Text("En esta seccion podras consultar tus gastos realizados", size=24, weight="bold"),
-            ft.ElevatedButton("Regresar al inicio", on_click=regresar, width=200, bgcolor="cyan", color = "black", icon=(ft.Icon(ft.Icons.HOME, color=ft.Colors.WHITE, size=25)))
+            ft.ElevatedButton("Regresar al inicio", on_click=regresar, width=200, bgcolor="cyan", color = "black", icon=(ft.Icon(ft.Icons.HOME, color=ft.Colors.WHITE, size=25))),
+            gasto
         ],
         spacing=20,
     )
