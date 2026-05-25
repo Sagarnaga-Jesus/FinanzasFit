48e0fe5b08ed78021d567165503c7542cdaf7576
Sagarnaga-Jesus <23308060610335@cetis61.edu.mx>
2026-05-25 10:28:31 -0500

    aer

--- src/views/PresupuestoView.py
+++ src/views/PresupuestoView.py
@@ -46,6 +46,13 @@ def PresupuestoView(page, controller):
         bgcolor=ft.Colors.GREEN,
         on_click=guardar2
     )
+    
+    eliminar = ft.IconButton(
+        ft.Icons.ADD,
+        tooltip="Eliminar presupuesto",
+        bgcolor=ft.Colors.GREEN,
+        on_click=guardar2
+    )
 
     card_dinero = ft.Card(
         content=ft.Container(
